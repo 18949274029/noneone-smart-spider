037006159d4e4f8f53aec63fa78ccee70c06822f
379106481@qq.com <lifeifei168>
2017-12-01 10:45:52 +0800

    csdn新规则

--- noneone-smart-job/src/main/java/com/noneone/webmagic/processor/CSDNRepoPageProcessor.java
+++ noneone-smart-job/src/main/java/com/noneone/webmagic/processor/CSDNRepoPageProcessor.java
@@ -2,6 +2,8 @@ package com.noneone.webmagic.processor;
 
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
@@ -47,13 +49,17 @@ public class CSDNRepoPageProcessor   implements PageProcessor,ProcessorEXE{
         // 列表页
         if (!page.getUrl().regex("http://blog\\.csdn\\.net/" + SPIDER_USERNAME + "/article/details/\\d+").match()) {
             // 添加所有文章页
-            page.addTargetRequests(page.getHtml().xpath("//div[@id='article_list']").links()// 限定文章列表获取区域
+//            page.addTargetRequests(page.getHtml().xpath("//div[@id='article_list']").links()// 限定文章列表获取区域
+//                    .regex("/" + SPIDER_USERNAME + "/article/details/\\d+")
+//                    .replace("/" + SPIDER_USERNAME + "/", "http://blog.csdn.net/" + SPIDER_USERNAME + "/")// 巧用替换给把相对url转换成绝对url
+//                    .all());
+//            // 添加其他列表页
+//            page.addTargetRequests(page.getHtml().xpath("//div[@id='papelist']").links()// 限定其他列表页获取区域
+//                    .regex("/" + SPIDER_USERNAME + "/article/list/\\d+")
+//                    .replace("/" + SPIDER_USERNAME + "/", "http://blog.csdn.net/" + SPIDER_USERNAME + "/")// 巧用替换给把相对url转换成绝对url
+//                    .all());
+            page.addTargetRequests(page.getHtml().xpath("//div[@class='content clearfix']").links()// 限定文章列表获取区域
                     .regex("/" + SPIDER_USERNAME + "/article/details/\\d+")
-                    .replace("/" + SPIDER_USERNAME + "/", "http://blog.csdn.net/" + SPIDER_USERNAME + "/")// 巧用替换给把相对url转换成绝对url
-                    .all());
-            // 添加其他列表页
-            page.addTargetRequests(page.getHtml().xpath("//div[@id='papelist']").links()// 限定其他列表页获取区域
-                    .regex("/" + SPIDER_USERNAME + "/article/list/\\d+")
                     .replace("/" + SPIDER_USERNAME + "/", "http://blog.csdn.net/" + SPIDER_USERNAME + "/")// 巧用替换给把相对url转换成绝对url
                     .all());
             // 文章页
@@ -61,39 +67,41 @@ public class CSDNRepoPageProcessor   implements PageProcessor,ProcessorEXE{
             
             // 用CsdnBlog类来存抓取到的数据，方便存入数据库
             Post post = new Post();
-            // 设置标题
             post.setContent(
                     deleteCSDNImage(page.getHtml().xpath("//div[@class='markdown_views']").get(),false));
+            if (StringUtils.isEmpty(post.getContent())) {
+            	 post.setContent(
+                         deleteCSDNImage(page.getHtml().xpath("//div[@class='article_content csdn-tracking-statistics']").get(),false));
+			}
             post.setAuthorId(AUTHORID);
             post.setEditor("spider-csdn");
-            post.setGroup(4);
+            post.setGroup(1);
             post.setStatus(0);
-            post.setTags("博客,CSDN,编程");
+            post.setTags("IT博文,IT,编程");
             post.setTitle(page.getHtml().xpath("//h1[@class='csdn_top']/allText()").get());
            String data = JSON.toJSONString(post);
 		try {
 			if (post.getTitle()==null||post.getContent()==null) {
 				return;
 			}
+//			System.out.println(JSON.toJSONString(post));
 			 AESUtil aes = new AESUtil("lifeifei11111111");
 			 data = aes.Encrypt(data);
-//             把对象存入数据库
             String responseJson = Http4ClientUtil.postForm(noneoneblogUrl, "post="+data );
             System.out.println("发布==>"+responseJson);
         
-            // 把对象输出控制台
-//            System.out.println(JSON.toJSONString(post));
+            System.out.println(JSON.toJSONString(post));
             if ("ok".equals(responseJson)) {
             	size++;// 文章数量加1
             	readSize++;
-            	if (size>=SPIDER_PAGETOTAL) {
+            	if (size>=SPIDER_PAGETOTAL-1) {
             		spider.stop();
 				} 
             	
 			}else{
 				readSize++;
 			}
-            if (readSize>50) {//读一百篇就不加载了
+            if (readSize>10000) {//读一百篇就不加载了
             	spider.stop();
 			}
 //            Thread.sleep(1000000000);
@@ -134,7 +142,8 @@ public class CSDNRepoPageProcessor   implements PageProcessor,ProcessorEXE{
     
     
     public static void main(String[] args) {
-      
+    	 ProcessorEXE exe = new CSDNRepoPageProcessor();
+       exe.executeProcessor("foamflower",100,15);
     }
 	@Override
 	public void executeProcessor(String userName,int pageTotal,int authorIds) {
@@ -147,7 +156,7 @@ public class CSDNRepoPageProcessor   implements PageProcessor,ProcessorEXE{
 	        System.out.println("【爬虫开始】请耐心等待一大波数据到你碗里来...");
 	        startTime = System.currentTimeMillis();
 	        // 从用户博客首页开始抓，开启5个线程，启动爬虫
-	        spider=  Spider.create(new CSDNRepoPageProcessor()).addUrl("http://blog.csdn.net/"+SPIDER_USERNAME ).thread(4);
+	        spider=  Spider.create(new CSDNRepoPageProcessor()).addUrl("http://blog.csdn.net/"+SPIDER_USERNAME ).thread(2);
 	        spider.run();
 	        endTime = System.currentTimeMillis();
 	        System.out.println("【爬虫结束】共抓取" + size + "篇文章，耗时约" + ((endTime - startTime) / 1000) + "秒，已保存到数据库，请查收！");
